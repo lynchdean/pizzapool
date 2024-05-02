8430eca9c67fa84d029888b7d8ac256067bdad38
Dean Lynch <lynchdean@gmail.ccom>
2024-05-02 14:49:04 +0100

    Full tests for pizzaslices methods

--- events/tests.py
+++ events/tests.py
@@ -131,11 +131,50 @@ class PizzaOrderModelTests(TestCase):
         self.event.locked = True
         self.assertTrue(self.order.event_is_locked())
 
-    def test_save_fails_if_event_locked(self):
+    def test_order_save_fails_if_event_locked(self):
         """
-        save() throws Validation error if event is locked
+        save() throws Validation error if event is locked.
         :return:
         """
         self.event.locked = True
         with self.assertRaisesRegex(ValidationError, "locked"):
             self.order.save()
+
+
+class PizzaSlicesModelTests(TestCase):
+    def setUp(self):
+        self.event = create_event()
+        self.order = create_order(event=self.event)
+
+    def tearDown(self):
+        self.event.delete()
+        self.order.delete()
+
+    def test_create_slices_fails_if_event_locked(self):
+        """
+        save() throws Validation error if event is locked.
+        :return:
+        """
+        self.event.locked = True
+        with self.assertRaisesRegex(ValidationError, "locked"):
+            create_slices(pizza_order=self.order)
+
+    def test_create_slices_fails_if_order_is_full(self):
+        """
+        save() throws Validation error if order is full.
+        :return:
+        """
+        available = self.order.get_total_remaining()
+        create_slices(pizza_order=self.order, number_of_slices=available)
+        with self.assertRaisesRegex(ValidationError, "Insufficient"):
+            create_slices(pizza_order=self.order, number_of_slices=1)
+
+    def test_create_slices_fails_if_order_doesnt_have_enough_slices(self):
+        """
+        save() throws Validation error if order doesn't have a sufficient number of slices remaining.
+        :return:
+        """
+        available = self.order.get_total_remaining()
+        create_slices(pizza_order=self.order, number_of_slices=available-1)
+        with self.assertRaisesRegex(ValidationError, "Insufficient"):
+            create_slices(pizza_order=self.order, number_of_slices=2)
