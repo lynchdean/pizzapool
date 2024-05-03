4653516fab593563d06cfbc5ebcb7e22d3a6f1b2
Dean Lynch <lynchdean@gmail.ccom>
2024-05-03 12:51:37 +0100

    use StaticLiveServerTestCase

--- events/test_selenium.py
+++ events/test_selenium.py
@@ -1,15 +1,36 @@
-import unittest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+
+from .testing_utils import create_event, create_order, create_slices
 
 
-class SeleniumTestCase(unittest.TestCase):
-    def setUp(self):
+class MySeleniumTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.event = create_event()
+        cls.order = create_order(event=cls.event)
+
         options = Options()
-        options.add_argument('--headless=new')
-        self.browser = webdriver.Chrome(options=options)
-        self.addCleanup(self.browser.quit)
+        options.add_argument("--headless")
+        cls.selenium = WebDriver(options=options)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        cls.event.delete()
+        cls.order.delete()
+        super().tearDownClass()
 
     def test_page_title(self):
-        self.browser.get('http://127.0.0.1:8000/events')
-        self.assertIn('pizzapool', self.browser.title)
+        self.selenium.get(f"{self.live_server_url}/events/")
+        self.assertIn('pizzapool', self.selenium.title)
+
+    # def test_order_creation(self):
+    #     self.browser.get(f'http://127.0.0.1:8000/events/{self.event.id}/')
+    #     time.sleep(10)
---
+++ events/testing_utils.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+
+from .models import Event, PizzaOrder, PizzaSlices
+
+
+def create_event(event_title="Test Event", date=timezone.now(), description="desc"):
+    return Event.objects.create(event_title=event_title, date=date, description=description)
+
+
+def create_order(event, purchaser_name="Bob", purchaser_whatsapp="0879876543", purchaser_revolut="BobRev",
+                 pizza_type="Pep", price_per_slice=4, available_slices=7):
+    return PizzaOrder.objects.create(event=event, purchaser_name=purchaser_name, purchaser_whatsapp=purchaser_whatsapp,
+                                     purchaser_revolut=purchaser_revolut, pizza_type=pizza_type,
+                                     price_per_slice=price_per_slice, available_slices=available_slices)
+
+
+def create_slices(pizza_order, buyer_name="John", buyer_whatsapp="0871234567", number_of_slices=1):
+    return PizzaSlices.objects.create(pizza_order=pizza_order, buyer_name=buyer_name, buyer_whatsapp=buyer_whatsapp,
+                                      number_of_slices=number_of_slices)
