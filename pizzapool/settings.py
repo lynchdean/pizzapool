152e124a15a6cf9bf5819d7cc06928f79a91ccbe
Dean Lynch <lynchdean@gmail.com>
2024-06-11 10:15:28 +0100

    DEDUG env var must be casted to bool

--- pizzapool/settings.py
+++ pizzapool/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
