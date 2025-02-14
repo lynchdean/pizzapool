a8d3aeb5496e6e7557a12b5968f1110d4018d4bf
Dean Lynch <lynchdean@gmail.com>
2025-02-14 16:23:46 +0000

    update Dockerfile, remove db from docker compose

--- pizzapool/urls.py
+++ pizzapool/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
                   path("", include("django.contrib.auth.urls")),
                   path('', include("events.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
